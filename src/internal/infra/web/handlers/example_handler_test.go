7ff52971d5437d13cdbfafb063c1ccf07c8e5267
Caique Ribeiro Rodrigues <ribeiro.caique95@gmail.com>
2024-10-24 11:29:07 -0300

    tests: tests for handler and middleware

--- src/internal/infra/web/handlers/example_handler_test.go
+++ src/internal/infra/web/handlers/example_handler_test.go
@@ -1,1 +1,32 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExampleHandler_Get(t *testing.T) {
+	handler := NewExampleHandler()
+
+	req := httptest.NewRequest(http.MethodGet, "/example", nil)
+	rr := httptest.NewRecorder()
+
+	handler.Get(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code, "expected status 200")
+
+	var response ExampleDto
+	expected := ExampleDto{
+		Message: "Ok",
+		Status:  200,
+	}
+
+	err := json.NewDecoder(rr.Body).Decode(&response)
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, response)
+}
---
+++ src/internal/infra/web/middlewares/ratelimiter_test.go
@@ -0,0 +1,89 @@
+package middlewares
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/CaiqueRibeiro/rate-limiter-challenge/src/pkg/ratelimiter/strategies"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type RateLimiterMock struct {
+	mock.Mock
+}
+
+func (m *RateLimiterMock) Check(ctx context.Context, r *http.Request) (*strategies.LimitResponse, error) {
+	args := m.Called(ctx, r)
+	return args.Get(0).(*strategies.LimitResponse), args.Error(1)
+}
+
+func TestRateLimiterMiddlewareHandleAllow(t *testing.T) {
+	mockLimiter := new(RateLimiterMock)
+	middleware := NewRateLimiterMiddleware(mockLimiter)
+
+	mockLimiter.On("Check", mock.Anything, mock.Anything).Return(&strategies.LimitResponse{
+		Result:    strategies.Allow,
+		Limit:     10,
+		Remaining: 5,
+		ExpiresAt: time.Now().Add(1 * time.Hour),
+	}, nil)
+
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("success"))
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/some-endpoint", nil)
+	rr := httptest.NewRecorder()
+
+	middleware.Handle(nextHandler).ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, "10", rr.Header().Get("X-RateLimit-Limit"))
+	assert.Equal(t, "5", rr.Header().Get("X-RateLimit-Remaining"))
+	assert.NotEmpty(t, rr.Header().Get("X-RateLimit-Reset"))
+	assert.Equal(t, "success", rr.Body.String())
+
+	mockLimiter.AssertExpectations(t)
+}
+
+func TestRateLimiterMiddlewareHandleDeny(t *testing.T) {
+	mockLimiter := new(RateLimiterMock)
+	middleware := NewRateLimiterMiddleware(mockLimiter)
+
+	mockLimiter.On("Check", mock.Anything, mock.Anything).Return(&strategies.LimitResponse{
+		Result:    strategies.Deny,
+		Limit:     int64(10),
+		Remaining: int64(0),
+		ExpiresAt: time.Now().Add(1 * time.Hour),
+	}, nil)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rr := httptest.NewRecorder()
+
+	middleware.Handle(http.NotFoundHandler()).ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusTooManyRequests, rr.Code)
+	assert.Contains(t, rr.Body.String(), "maximum number of requests")
+	mockLimiter.AssertExpectations(t)
+}
+
+func TestRateLimiterMiddleware_Handle_InternalServerError(t *testing.T) {
+	mockLimiter := new(RateLimiterMock)
+	middleware := NewRateLimiterMiddleware(mockLimiter)
+
+	mockLimiter.On("Check", mock.Anything, mock.Anything).Return(nil, http.ErrHandlerTimeout)
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rr := httptest.NewRecorder()
+
+	middleware.Handle(http.NotFoundHandler()).ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusInternalServerError, rr.Code)
+	assert.Contains(t, rr.Body.String(), http.ErrHandlerTimeout.Error())
+	mockLimiter.AssertExpectations(t)
+}
